55d8d3773e05ec55684cced004f331d2c71adb2d
YoshifumiSato <yoshifumi0521@gmail.com>
2012-09-17 02:17:16 +0900

    helperのテストコードを追加

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,9 @@
 module ArticlesHelper
+
+  def posted_on(article)
+    time = article.created_at
+    "#{time.year}/#{time.month}/#{time.day}"
+
+  end
+
 end
--- spec/helpers/articles_helper_spec.rb
+++ spec/helpers/articles_helper_spec.rb
@@ -1,3 +1,4 @@
+#coding:utf-8
 require 'spec_helper'
 
 # Specs in this file have access to a helper object that includes
@@ -11,5 +12,24 @@ require 'spec_helper'
 #   end
 # end
 describe ArticlesHelper do
-  #pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "#posted_on" do
+    before do
+      @now = Time.now
+      @article = Article.create(
+        :title => "First Post",
+        :body => "Hello"
+      )
+    end
+
+    it {
+      helper.posted_on(@article).should == 
+      "#{@now.year}/#{@now.month}/#{@now.day}"
+    }
+  end
+
+
+
+
+
 end
